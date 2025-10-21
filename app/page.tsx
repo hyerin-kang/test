2430f42cacac6ce4a138cf2675d45d3008f65ef2
[이모션]강혜린 <hyerin.kang@emotion.co.kr>
2025-10-21 18:01:52 +0900

    test

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main className="w-full bg-amber-400 p-10 text-center">test</main>
+    </div>
+  );
+}
