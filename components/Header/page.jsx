73e4ce40fb16340ddfe79e49a9e06707daa6cfb7
[이모션]강혜린 <hyerin.kang@emotion.co.kr>
2025-10-30 16:37:30 +0900

    header-search

--- components/Header/page.jsx
+++ components/Header/page.jsx
@@ -1,20 +1,30 @@
 "use client";
 import Image from "next/image";
 import "./header.scss";
-import HeaderLogo from "@/public/images/common/header-logo.svg";
 import { menu } from "./menu.js";
 import { useState } from "react";
 import GnbMenu from "./GnbMenu/page";
+import Link from "next/link";
+import IcoSearchWh from "@/public/images/icon/ico-search-white.svg";
+import IcoSearchBlack from "@/public/images/icon/ico-search-black.svg";
+import IcoCloseBk from "@/public/images/icon/ico-close-bk.svg";
+import { motion, AnimatePresence } from "framer-motion";
+import { searchArea } from "@/lib/framerMotion";
 
 const Header = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const headerClass = `
+    header 
+    ${hoveredIndex !== null ? "open" : ""} 
+    ${isSearchOpen ? "open-search" : ""}
+  `;
 
   return (
-    <div className="header">
+    <div className={`header ${headerClass}`}>
       <div className="left">
-        <a href="/">
-          <Image src={HeaderLogo} alt="header-logo" width={140} height={40} />
-        </a>
+        <a href="/" className="logo"></a>
         <ul className="nav">
           {menu.data.map((item, index) => {
             return (
@@ -43,7 +53,59 @@ const Header = () => {
           })}
         </ul>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <ul>
+          <li>
+            <Link href="/">
+              <span>ESG</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <span>Contact us</span>
+            </Link>
+          </li>
+          <li className="changeLan">언어변경</li>
+          <li className="search-btn">
+            <button
+              onClick={() => {
+                setIsSearchOpen(!isSearchOpen);
+              }}
+            >
+              {isSearchOpen ? (
+                <Image src={IcoCloseBk} alt="ico-close-btn" />
+              ) : (
+                <Image src={IcoSearchWh} alt="ico-search-btn" />
+              )}
+            </button>
+            {/* 검색버튼 클릭시 */}
+
+            <AnimatePresence mode="wait">
+              {isSearchOpen && (
+                <motion.div
+                  className="search-area"
+                  variants={searchArea}
+                  initial="initial"
+                  animate="enter"
+                  exit="exit"
+                >
+                  <div className="input-box">
+                    <input
+                      id="searchInput"
+                      name="searchInput"
+                      type="text"
+                      placeholder="검색어를 입력하세요"
+                    />
+                    <button>
+                      <Image src={IcoSearchBlack} alt="ico-search-btn" />
+                    </button>
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
