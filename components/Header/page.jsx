41c00203742f258be361ed1185b37b4986dc8ae0
[이모션]강혜린 <hyerin.kang@emotion.co.kr>
2025-10-31 09:45:20 +0900

    헤더 구조 개선 및 검색 페이지 추가

--- components/Header/page.jsx
+++ components/Header/page.jsx
@@ -21,90 +21,96 @@ const Header = () => {
     ${isSearchOpen ? "open-search" : ""}
   `;
 
+  const handleSearchPage = () => {
+    router.push("/search");
+  };
+
   return (
     <div className={`header ${headerClass}`}>
-      <div className="left">
-        <a href="/" className="logo"></a>
-        <ul className="nav">
-          {menu.data.map((item, index) => {
-            return (
-              <li
-                key={index}
-                className="nav-item"
-                onMouseOver={() => {
-                  setHoveredIndex(index);
-                }}
-                onMouseLeave={() => {
-                  setHoveredIndex(null);
+      <div className="header-inner">
+        <div className="left">
+          <a href="/" className="logo"></a>
+          <ul className="nav">
+            {menu.data.map((item, index) => {
+              return (
+                <li
+                  key={index}
+                  className="nav-item"
+                  onMouseOver={() => {
+                    setHoveredIndex(index);
+                  }}
+                  onMouseLeave={() => {
+                    setHoveredIndex(null);
+                  }}
+                >
+                  {/* 적용분야, 기술, 제품 ... */}
+                  <span>{item.value}</span>
+                  {hoveredIndex === index && (
+                    <div className="gnb-menu">
+                      <GnbMenu
+                        gnbMenuName={item.value}
+                        gnbMenuData={item.depth}
+                      />
+                    </div>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="right">
+          <ul>
+            <li>
+              <Link href="/">
+                <span>ESG</span>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <span>Contact us</span>
+              </Link>
+            </li>
+            <li className="changeLan">언어변경</li>
+            <li className="search-btn">
+              <button
+                onClick={() => {
+                  setIsSearchOpen(!isSearchOpen);
                 }}
               >
-                {/* 적용분야, 기술, 제품 ... */}
-                <span>{item.value}</span>
-                {hoveredIndex === index && (
-                  <div className="gnb-menu">
-                    <GnbMenu
-                      gnbMenuName={item.value}
-                      gnbMenuData={item.depth}
-                    />
-                  </div>
+                {isSearchOpen ? (
+                  <Image src={IcoCloseBk} alt="ico-close-btn" />
+                ) : (
+                  <Image src={IcoSearchWh} alt="ico-search-btn" />
                 )}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="right">
-        <ul>
-          <li>
-            <Link href="/">
-              <span>ESG</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <span>Contact us</span>
-            </Link>
-          </li>
-          <li className="changeLan">언어변경</li>
-          <li className="search-btn">
-            <button
-              onClick={() => {
-                setIsSearchOpen(!isSearchOpen);
-              }}
-            >
-              {isSearchOpen ? (
-                <Image src={IcoCloseBk} alt="ico-close-btn" />
-              ) : (
-                <Image src={IcoSearchWh} alt="ico-search-btn" />
-              )}
-            </button>
-            {/* 검색버튼 클릭시 */}
+              </button>
+              {/* 검색버튼 클릭시 */}
 
-            <AnimatePresence mode="wait">
-              {isSearchOpen && (
-                <motion.div
-                  className="search-area"
-                  variants={searchArea}
-                  initial="initial"
-                  animate="enter"
-                  exit="exit"
-                >
-                  <div className="input-box">
-                    <input
-                      id="searchInput"
-                      name="searchInput"
-                      type="text"
-                      placeholder="검색어를 입력하세요"
-                    />
-                    <button>
-                      <Image src={IcoSearchBlack} alt="ico-search-btn" />
-                    </button>
-                  </div>
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </li>
-        </ul>
+              <AnimatePresence mode="wait">
+                {isSearchOpen && (
+                  <motion.div
+                    className="search-area"
+                    variants={searchArea}
+                    initial="initial"
+                    animate="enter"
+                    exit="exit"
+                  >
+                    <div className="input-box">
+                      <input
+                        id="searchInput"
+                        name="searchInput"
+                        type="text"
+                        placeholder="검색어를 입력하세요"
+                      />
+                      <button onClick={handleSearchPage}>
+                        <Image src={IcoSearchBlack} alt="ico-search-btn" />
+                      </button>
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
