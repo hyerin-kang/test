12831a9deda3da2f941c7d1d6c503640a914b76c
[이모션]강혜린 <hyerin.kang@emotion.co.kr>
2025-11-05 15:42:21 +0900

    자동차 탭 및 핀 데이터 스타일 개선, 애니메이션 추가

--- app/applications/automotive/components/AutomotiveContent.jsx
+++ app/applications/automotive/components/AutomotiveContent.jsx
@@ -6,6 +6,7 @@ import CarPNG from "@/public/images/content/automotiveCarBg.png";
 import InteriorBGPNG from "@/public/images/content/interiorBG.png";
 import Image from "next/image";
 import { AUTOMOTIVE_PINS } from "../data/automotivePins";
+import "./../automotive.scss";
 import Link from "next/link";
 
 const TABS = [
@@ -18,31 +19,36 @@ const TABS = [
 ];
 export const AutomotiveContent = () => {
   const [tab, setTab] = useState("exterior");
-  const [popup, setPopup] = useState();
+  const [popup, setPopup] = useState("");
+  console.log(popup, "popup");
   return (
     <div id="AutomotiveContent">
       <TabArea tab={tab} setTab={setTab} tabs={TABS} />
       <div className="tab-bg-img">
         <div className="pins-area">
           {AUTOMOTIVE_PINS[tab]?.map((pin) => (
-            <div className="pin-group" key={pin.id}>
-              <div
-                className="ani-circle"
-                onClick={() => setPopup(pin.id)}
-                onMouseup={() => setPopup(pin.id)}
-                onMouseLeave={() => setPopup("")}
-              >
-                <div className="circle"></div>
-              </div>
+            <div
+              className={`pin-point ${pin.positionClass}`}
+              key={pin.id}
+              onMouseEnter={() => {
+                setPopup(pin.id);
+              }}
+              onMouseLeave={() => {
+                setPopup("");
+              }}
+            >
+              <div className="circle"></div>
+              {popup === pin.id && (
+                <Link href={pin.href} key={pin.id} className="pin">
+                  <p className="title">{pin.title}</p>
+                  <ul className="pin-list">
+                    {pin.list.map((list, index) => (
+                      <li key={index}>{list}</li>
+                    ))}
+                  </ul>
+                </Link>
+              )}
             </div>
-            // <Link href={pin.href} key={pin.id} className="pin">
-            //   <p className="title">{pin.title}</p>
-            //   <ul className="pin-list">
-            //     {pin.list.map((list, index) => (
-            //       <li key={index}>{list}</li>
-            //     ))}
-            //   </ul>
-            // </Link>
           ))}
         </div>
         <Image
--- app/applications/automotive/components/TabArea.jsx
+++ app/applications/automotive/components/TabArea.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./../automotive.scss";
 
 export const TabArea = ({ tab, setTab, tabs }) => {
