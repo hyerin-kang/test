a4c9eb844506c85edd4dd5a6c14ee5fda6418219
[이모션]강혜린 <hyerin.kang@emotion.co.kr>
2025-11-06 12:13:14 +0900

    feat: add axios instance for API requests with interceptors

--- app/applications/automotive/components/NewsArea.jsx
+++ app/applications/automotive/components/NewsArea.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+import useSWR from "swr";
+import { axiosInstance } from "../../../../lib/axiosInstance";
+import Link from "next/link";
 
+const fetcher = (url) => axiosInstance.get(url);
 export const NewsArea = () => {
-  return <div>NewsArea</div>;
+  const {
+    isLoading,
+    data: newsData,
+    error,
+  } = useSWR(`/kr/application/news/automotive`, fetcher);
+  console.log(newsData, "newsData");
+  if (isLoading) {
+    return (
+      <div>
+        <p>로딩 중...</p>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <p style={{ color: "red" }}>에러: {error.message}</p>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <p className="content-title">Related News</p>
+      {newsData.data?.map((item) => (
+        <Link
+          href={`/kr/media/newsroom/${item.mediaCenterMasterId}`}
+          key={item.mediaCenterMasterId}
+          className="border p-4 block"
+        >
+          <p>{item.category}</p>
+          <p>{item.title}</p>
+          <p>{item.createDateTime}</p>
+        </Link>
+      ))}
+    </div>
+  );
 };
