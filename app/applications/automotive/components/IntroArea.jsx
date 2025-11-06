3baced4079ba468365f8eb1eb9223e54d39d1f0c
[이모션]강혜린 <hyerin.kang@emotion.co.kr>
2025-11-06 10:15:01 +0900

    Refactor code structure for improved readability and maintainability

--- app/applications/automotive/components/IntroArea.jsx
+++ app/applications/automotive/components/IntroArea.jsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import { TextReveal } from "../../../../components/TextReveal/page";
+import Image from "next/image";
+import InteriorPNG from "@/public/images/content/automotive-interior.png";
+import ExteriorPNG from "@/public/images/content/automotive-exterior.png";
+import ArrowIcon from "@/public/images/icon/ico-link-arrow.svg";
 
 export const IntroArea = () => {
   return (
     <div>
-      <p>IntroArea</p>
       <TextReveal>
         <div className="flex md:flex-row flex-col-reverse md:p-2.5 pl-5 py-2.5 border-b border-[#30AE56] border-opacity-[0.2]">
           <Link
@@ -12,9 +15,7 @@ export const IntroArea = () => {
             className="arr-icon max-w-[700px] w-full md:p-10 pr-5 py-5 flex md:flex-col flex-row justify-between"
           >
             <div>
-              <p className="md:text-h1 text-body1 font-inter sm:font-normal font-medium">
-                Exterior
-              </p>
+              <p className="content-title">Exterior</p>
               <div className="md:text-subtext text-button flex md:gap-[138px] gap-[58px] md:mt-14 mt-10">
                 <div>
                   <p className="font-bold">Key Factor</p>
@@ -37,19 +38,19 @@ export const IntroArea = () => {
             </div>
 
             <div>
-              {/* <Image
+              <Image
                 src={ArrowIcon}
                 alt="right_arrow"
                 className="md:w-[46px] w-[22px]"
-              /> */}
+              />
             </div>
           </Link>
           <div className="flex-1 min-w-[50%]">
-            {/* <Image
+            <Image
               src={ExteriorPNG}
               alt="ExteriorPNG"
               className="w-full h-full"
-            /> */}
+            />
           </div>
         </div>
       </TextReveal>
@@ -60,9 +61,7 @@ export const IntroArea = () => {
             className="arr-icon max-w-[700px] w-full md:p-10 pr-5 py-5 flex md:flex-col flex-row justify-between"
           >
             <div>
-              <p className="md:text-h1 text-body1 font-inter sm:font-normal font-medium">
-                Interior
-              </p>
+              <p className="content-title">Interior</p>
               <div className="md:text-subtext text-button flex md:gap-[138px] gap-[58px] md:mt-14 mt-10">
                 <div>
                   <p className="font-bold">Key Factor</p>
@@ -89,19 +88,19 @@ export const IntroArea = () => {
             </div>
 
             <div>
-              {/* <Image
+              <Image
                 src={ArrowIcon}
                 alt="right_arrow"
                 className="md:w-[46px] w-[22px]"
-              /> */}
+              />
             </div>
           </Link>
           <div className="flex-1 min-w-[50%]">
-            {/* <Image
+            <Image
               src={InteriorPNG}
               alt="InteriorPNG"
               className="w-full h-full"
-            /> */}
+            />
           </div>
         </div>
       </TextReveal>
