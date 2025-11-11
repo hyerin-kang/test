9aa61709a142c5d509cc5ce89adf78035b46bef0
[이모션]강혜린 <hyerin.kang@emotion.co.kr>
2025-11-11 10:43:35 +0900

    ✨ 세부상품 슬라이드 적용

--- app/applications/automotive/exterior/page.jsx
+++ app/applications/automotive/exterior/page.jsx
@@ -6,27 +6,18 @@ import { useContext, useState } from "react";
 import { MenuContext } from "../../../../context/MenuContext";
 import Image from "next/image";
 import { TextReveal } from "../../../../components/TextReveal/page";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+
 import Link from "next/link";
-// 또는
-// import { useSWR } from "swr";
+import SlideComponent from "./components/SlideComponent";
 
 const fetcher = (url) => axiosInstance.get(url).then((res) => res);
 
 export default function page() {
-  const [swiperRef, setSwiperRef] = useState(null);
-  const [index, setIndex] = useState(1);
   const { menuData } = useContext(MenuContext);
   const { data: exteriorList, error } = useSWR(
     "/application/automotive/exterior",
     fetcher
   );
-
-  console.log(exteriorList, "exteriorList");
 
   return (
     <div className="pt-25">
@@ -49,35 +40,8 @@ export default function page() {
           <TextReveal key={item.key} className="border-t border-t-green-400">
             <p className="content-title">{item.title}</p>
             <p>{item.desc}</p>
-            <Swiper
-              ref={swiperRef}
-              onSwiper={(swiper) => setSwiperRef(swiper)}
-              onSlideChange={(swiper) => {
-                setSwiperRef(swiper);
-                setIndex(swiper?.activeIndex + 1);
-              }}
-              modules={[Pagination, Navigation]}
-            >
-              {item.child?.map((product) => {
-                return (
-                  <SwiperSlide>
-                    <Link key={product.key} href={product.path}>
-                      {product.title}
 
-                      {product.image ? (
-                        <Image
-                          src={product.image}
-                          alt={product.title}
-                          width={320}
-                          height={0}
-                          //   className="w-full h-full object-contain"
-                        />
-                      ) : null}
-                    </Link>
-                  </SwiperSlide>
-                );
-              })}
-            </Swiper>
+            <SlideComponent data={item?.child} keyName={item?.key} />
           </TextReveal>
         );
       })}
